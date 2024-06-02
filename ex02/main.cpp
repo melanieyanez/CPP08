238bc02281e4655613d9a0443ea21f36ceb65229
Melanie Yanez <melanieyanez@gmail.com>
2024-06-02 15:52:33 +0200

    ex02 clean

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: melanieyanez <melanieyanez@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 15:27:51 by melanieyane       #+#    #+#             */
-/*   Updated: 2024/06/02 15:29:41 by melanieyane      ###   ########.fr       */
+/*   Updated: 2024/06/02 15:52:24 by melanieyane      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,88 +16,88 @@
 
 template <typename T>
 void displayStack(const MutantStack<T> &stack) {
-    for (typename MutantStack<T>::const_iterator it = stack.cbegin(); it != stack.cend(); ++it) {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
+	for (typename MutantStack<T>::const_iterator it = stack.cbegin(); it != stack.cend(); ++it) {
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
 }
 
 void myTest()
 {
 	std::cout << "Testing default constructor" << std::endl;
-    MutantStack<int> mutant0;
-    std::cout << "Empty stack created. Size: " << mutant0.size() << std::endl;
+	MutantStack<int> mutant0;
+	std::cout << "Empty stack created. Size: " << mutant0.size() << std::endl;
 	
-    std::cout << "******************************************************" << std::endl;
-    std::cout << "Testing inherited member functions" << std::endl;
-    mutant0.push(1);
-    mutant0.push(3);
-    mutant0.push(21);
-    mutant0.push(42);
-    std::cout << "Stack after pushes: ";
-    displayStack(mutant0);
-    std::cout << "mutant0.size() = " << mutant0.size() << std::endl;
-    std::cout << "mutant0.top() = " << mutant0.top() << std::endl;
-    mutant0.pop();
-    std::cout << "Stack after pop: ";
-    displayStack(mutant0);
-    std::cout << "mutant0.top() = " << mutant0.top() << std::endl;
+	std::cout << "******************************************************" << std::endl;
+	std::cout << "Testing inherited member functions" << std::endl;
+	mutant0.push(1);
+	mutant0.push(3);
+	mutant0.push(21);
+	mutant0.push(42);
+	std::cout << "Stack after pushes: ";
+	displayStack(mutant0);
+	std::cout << "mutant0.size() = " << mutant0.size() << std::endl;
+	std::cout << "mutant0.top() = " << mutant0.top() << std::endl;
+	mutant0.pop();
+	std::cout << "Stack after pop: ";
+	displayStack(mutant0);
+	std::cout << "mutant0.top() = " << mutant0.top() << std::endl;
 	
-    std::cout << "******************************************************" << std::endl;
-    std::cout << "Testing iterator of MutantStack" << std::endl;
-    std::cout << "Stack elements using iterator: ";
-    displayStack(mutant0);
-    std::cout << "Testing iterator accessing specific element" << std::endl;
-    MutantStack<int>::iterator it = mutant0.begin();
-    std::advance(it, 1); // advancing iterator to second element
-    std::cout << "*(it + 1) = " << *it << std::endl;
+	std::cout << "******************************************************" << std::endl;
+	std::cout << "Testing iterator of MutantStack" << std::endl;
+	std::cout << "Stack elements using iterator: ";
+	displayStack(mutant0);
+	std::cout << "Testing iterator accessing specific element" << std::endl;
+	MutantStack<int>::iterator it = mutant0.begin();
+	std::advance(it, 1);
+	std::cout << "*(it + 1) = " << *it << std::endl;
 	
-    std::cout << "******************************************************" << std::endl;
-    std::cout << "Testing operator= overload" << std::endl;
-    MutantStack<int> mutant1;
-    mutant1 = mutant0;
-    std::cout << "Stack after assignment: ";
-    displayStack(mutant1);
+	std::cout << "******************************************************" << std::endl;
+	std::cout << "Testing operator= overload" << std::endl;
+	MutantStack<int> mutant1;
+	mutant1 = mutant0;
+	std::cout << "Stack after assignment: ";
+	displayStack(mutant1);
 	
-    std::cout << "******************************************************" << std::endl;
-    std::cout << "Testing copy constructor" << std::endl;
-    MutantStack<int> mutant2(mutant1);
-    std::cout << "Stack after copy construction: ";
-    displayStack(mutant2);
+	std::cout << "******************************************************" << std::endl;
+	std::cout << "Testing copy constructor" << std::endl;
+	MutantStack<int> mutant2(mutant1);
+	std::cout << "Stack after copy construction: ";
+	displayStack(mutant2);
 	
-    std::cout << "******************************************************" << std::endl;
-    std::cout << "Testing reverse iterator of MutantStack" << std::endl;
-    std::cout << "Stack elements in reverse order: ";
-    for (MutantStack<int>::reverse_iterator rit = mutant2.rbegin(); rit != mutant2.rend(); ++rit) {
-        std::cout << *rit << " ";
-    }
-    std::cout << std::endl;
+	std::cout << "******************************************************" << std::endl;
+	std::cout << "Testing reverse iterator of MutantStack" << std::endl;
+	std::cout << "Stack elements in reverse order: ";
+	for (MutantStack<int>::reverse_iterator rit = mutant2.rbegin(); rit != mutant2.rend(); ++rit) {
+		std::cout << *rit << " ";
+	}
+	std::cout << std::endl;
 	
-    std::cout << "******************************************************" << std::endl;
-    std::cout << "Testing const iterator of MutantStack" << std::endl;
-    const MutantStack<int> mutantConst(mutant2);
-    std::cout << "Const stack elements: ";
-    for (MutantStack<int>::const_iterator cit = mutantConst.cbegin(); cit != mutantConst.cend(); ++cit) {
-        std::cout << *cit << " ";
-    }
-    std::cout << std::endl;
+	std::cout << "******************************************************" << std::endl;
+	std::cout << "Testing const iterator of MutantStack" << std::endl;
+	const MutantStack<int> mutantConst(mutant2);
+	std::cout << "Const stack elements: ";
+	for (MutantStack<int>::const_iterator cit = mutantConst.cbegin(); cit != mutantConst.cend(); ++cit) {
+		std::cout << *cit << " ";
+	}
+	std::cout << std::endl;
 	
-    std::cout << "******************************************************" << std::endl;
-    std::cout << "Testing const reverse iterator of MutantStack" << std::endl;
-    std::cout << "Const stack elements in reverse order: ";
-    for (MutantStack<int>::const_reverse_iterator crit = mutantConst.crbegin(); crit != mutantConst.crend(); ++crit) {
-        std::cout << *crit << " ";
-    }
-    std::cout << std::endl;
+	std::cout << "******************************************************" << std::endl;
+	std::cout << "Testing const reverse iterator of MutantStack" << std::endl;
+	std::cout << "Const stack elements in reverse order: ";
+	for (MutantStack<int>::const_reverse_iterator crit = mutantConst.crbegin(); crit != mutantConst.crend(); ++crit) {
+		std::cout << *crit << " ";
+	}
+	std::cout << std::endl;
 	
-    std::cout << "******************************************************" << std::endl;
-    std::cout << "Testing large number of elements" << std::endl;
-    MutantStack<int> largeStack;
-    for (int i = 0; i < 10000; ++i) {
-        largeStack.push(i);
-    }
-    std::cout << "Large stack created. Size: " << largeStack.size() << std::endl;
-    std::cout << "Top element of large stack: " << largeStack.top() << std::endl;
+	std::cout << "******************************************************" << std::endl;
+	std::cout << "Testing large number of elements" << std::endl;
+	MutantStack<int> largeStack;
+	for (int i = 0; i < 10000; ++i) {
+		largeStack.push(i);
+	}
+	std::cout << "Large stack created. Size: " << largeStack.size() << std::endl;
+	std::cout << "Top element of large stack: " << largeStack.top() << std::endl;
 	return;
 }
 
@@ -127,10 +127,11 @@ void subjectTest()
 	--it;
 	while (it != ite)
 	{
-    	std::cout << *it << std::endl;
+		std::cout << *it << std::endl;
 		++it;
 	}
 	std::stack<int> s(mstack);
+	return;
 }
 
 void listTest()
@@ -159,10 +160,11 @@ void listTest()
 	--it;
 	while (it != ite)
 	{
-    	std::cout << *it << std::endl;
+		std::cout << *it << std::endl;
 		++it;
 	}
 	std::list<int> s(mstack);
+	return;
 }
 
 int main()
